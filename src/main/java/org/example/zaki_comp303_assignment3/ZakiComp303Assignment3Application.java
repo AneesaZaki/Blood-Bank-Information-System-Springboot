c71db71d6d20916e4ba03bb4c901e294ef87ed72
Aneesa <aneesa@Aneesas-MacBook-Pro.local>
2024-11-14 21:48:08 -0500

    tested REST APIs

---
+++ src/main/java/org/example/zaki_comp303_assignment3/ZakiComp303Assignment3Application.java
@@ -0,0 +1,13 @@
+package org.example.zaki_comp303_assignment3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZakiComp303Assignment3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZakiComp303Assignment3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/zaki_comp303_assignment3/ZakiComp303Assignment3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.zaki_comp303_assignment3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZakiComp303Assignment3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
